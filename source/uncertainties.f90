06a617e58c9f7813b0bd328ab8f7457623cc20a4
Bryce <b3cousins@gmail.com>
2018-07-24 18:37:59 -0400

    Update uncertainties.f90
    Changed the window size for computing continuum RMS from 20 to 40, discarding the 10 highest values of the 40 values, instead of the 2 highest of 20 values.

--- source/uncertainties.f90
+++ source/uncertainties.f90
@@ -14,9 +14,12 @@ subroutine get_uncertainties(fittedspectrum, realspec, fittedlines)
   type(spectrum), dimension(:), allocatable :: realspec, fittedspectrum
   type(linelist), dimension(:), allocatable :: fittedlines
   real, dimension(:), allocatable :: residuals
-  real, dimension(20) :: spectrumchunk
+!  real, dimension(20) :: spectrumchunk     !BSC 060318 - see below
   real :: wavelengthsampling
   integer :: i, uncertaintywavelengthindex
+!BSC 060318 - see later note about windows
+  real :: wind
+  real, dimension(40) :: spectrumchunk
 
 #ifdef CO
   print *,"subroutine: get_uncertainties"
@@ -30,16 +33,25 @@ subroutine get_uncertainties(fittedspectrum, realspec, fittedlines)
 ! 2 largest (this avoids the uncertainty calculation being biased by unfitted
 ! lines or large residuals from the wings of lines)
 
-  do i=10,size(realspec)-10
-    spectrumchunk=abs(residuals(i-9:i+10))
+!BSC 060318 - changed the 20 unit window to 40, discarding the 10 highest values
+  do i=20,size(realspec)-20
+    spectrumchunk=abs(residuals(i-19:i+20))
     call qsort(spectrumchunk)
-    realspec(i)%uncertainty=(sum(spectrumchunk(1:18)**2)/18.)**0.5
+    realspec(i)%uncertainty=(sum(spectrumchunk(1:30)**2)/30.)**0.5
   enddo
+!  do i=10,size(realspec)-10
+!    spectrumchunk=abs(residuals(i-9:i+10))
+!    call qsort(spectrumchunk)
+!    realspec(i)%uncertainty=(sum(spectrumchunk(1:18)**2)/18.)**0.5
+!  enddo
 
 ! fill in the ends with the closest calculated values
+! BSC 060318 - changed below to reflect window changes
+  realspec(1:20)%uncertainty=realspec(21)%uncertainty
+  realspec(size(realspec%uncertainty)-20:size(realspec%uncertainty))%uncertainty=realspec(size(realspec%uncertainty)-21)%uncertainty
 
-  realspec(1:10)%uncertainty=realspec(11)%uncertainty
-  realspec(size(realspec%uncertainty)-10:size(realspec%uncertainty))%uncertainty=realspec(size(realspec%uncertainty)-11)%uncertainty
+!  realspec(1:10)%uncertainty=realspec(11)%uncertainty
+!  realspec(size(realspec%uncertainty)-10:size(realspec%uncertainty))%uncertainty=realspec(size(realspec%uncertainty)-11)%uncertainty
 
 ! determine uncertainty for each line from ratio of peak flux to rms at wavelength of line, using relation from Lenz & Ayres, 1992, PASP, 104, 1104
 ! wavelength sampling determined at location of line. this would break if a line peak is in the last pixel of the spectrum, should add something to prevent this as it would only have half the profile to fit from anyway in that case.
@@ -50,6 +62,7 @@ subroutine get_uncertainties(fittedspectrum, realspec, fittedlines)
       wavelengthsampling = realspec(uncertaintywavelengthindex+1)%wavelength - realspec(uncertaintywavelengthindex)%wavelength
       fittedlines(i)%uncertainty=0.67*(fittedlines(i)%wavelength/(fittedlines(i)%resolution*wavelengthsampling))**0.5&
       &*fittedlines(i)%peak/realspec(uncertaintywavelengthindex)%uncertainty
+!      write (6, "(I5,2x,I5,2x,F7.1,2x,ES11.3E2)") i, uncertaintywavelengthindex, fittedlines(i)%wavelength, realspec(uncertaintywavelengthindex)%uncertainty   !BSC 060318 - readout for bugfixing
     else
       fittedlines(i)%uncertainty=0.d0
     endif
